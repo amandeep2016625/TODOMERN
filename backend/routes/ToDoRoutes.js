766e1c56ae4dca018cbf2ff6ac67e80131e5a556
amandeep2016625 <yadav.amandeep.2016625@gmail.com>
2024-06-23 19:24:17 +0530

    working

---
+++ backend/routes/ToDoRoutes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const {
+  getToDos,
+  saveToDo,
+  updateToDo,
+  deleteToDo,
+} = require("../controller/ToDoController");
+
+const router = Router();
+
+
+//all the routes 
+router.get("/get", getToDos);
+router.post("/save", saveToDo);
+router.put("/update/:id", updateToDo);
+router.delete("/delete/:id", deleteToDo);
+
+module.exports = router;
